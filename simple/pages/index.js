fb0e0347b842465a9ef066ab3dfb2b584c58f552
sersoong <sersoong@gmail.com>
2018-09-17 10:07:01 +0800

    Add a simple example

---
+++ simple/pages/index.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react'
+import { connect } from "react-redux";
+
+class Page extends Component {
+  static getInitialProps({store,isServer,pathname, query}) {
+    store.dispatch({type:'Foo',payload:'foo'})        
+    return {custom: 'test by sersoong'}; // pass some custom props to component
+  }
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <div>Prop from Redux {this.props.foo}</div>
+        <div>Prop from getInitialProps {this.props.custom}</div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  const {foo}  = state
+  return {foo}
+}
+
+export default connect(mapStateToProps)(Page)
+
+
---
+++ simple/store.js
@@ -0,0 +1,17 @@
+import React,{ Component } from "react";
+import { createStore } from "redux";
+
+const reducer = (state={foo:''},action) =>{
+    switch (action.type){
+        case 'FOO':
+            return {...state,foo:action.payload}
+        default:
+            return state
+    }
+}
+
+const initialState = (initialState) =>{
+    return createStore(reducer,initialState)
+}
+
+export default initialState
